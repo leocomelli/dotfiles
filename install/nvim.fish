4e761638618e99675804c2e99155ea6fa24415a9
Leonardo Comelli <leonardo.comelli@gmail.com>
2023-08-03 23:26:05 -0300

    Change to use Packer to manager plugins

--- install/nvim.fish
+++ install/nvim.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/env fish
 
-# Vim plugin manager
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/plugged/Vundle.vim
-
 asdf plugin-add nodejs
 asdf install nodejs 14.17.5
 asdf global nodejs 14.17.5
@@ -11,5 +8,4 @@ npm install --global yarn
 
 python3 -m pip install --user --upgrade pynvim
 
-vim +PluginInstall +qall
-
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
