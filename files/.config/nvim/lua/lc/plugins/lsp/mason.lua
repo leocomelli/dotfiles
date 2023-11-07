711d19e0d4824e8af7937e449fc034d4a23adc8f
Leonardo Comelli <leonardo.comelli@gmail.com>
2023-11-07 15:42:31 -0300

    Add goimports

--- files/.config/nvim/lua/lc/plugins/lsp/mason.lua
+++ files/.config/nvim/lua/lc/plugins/lsp/mason.lua
@@ -55,6 +55,7 @@ mason_null_ls.setup({
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
 		"eslint_d", -- ts/js linter
+		"goimports", -- go formatter
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
